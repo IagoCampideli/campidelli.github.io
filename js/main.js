8e003d1ec03b65ac7a4f5f89518ff9e3e9b0ff8f
Iago Campideli <86387130+IagoCampideli@users.noreply.github.com>
2021-11-10 18:54:05 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,86 @@
+function findOffset(element) {
+  var top = 0,
+    left = 0;
+
+  do {
+    top += element.offsetTop || 0;
+    left += element.offsetLeft || 0;
+    element = element.offsetParent;
+  } while (element);
+
+  return {
+    top: top,
+    left: left,
+  };
+}
+
+window.onload = function () {
+  var stickyHeader = document.getElementById("header");
+  var headerOffset = findOffset(stickyHeader);
+
+  window.onscroll = function () {
+    // body.scrollTop is deprecated and no longer available on Firefox
+    var bodyScrollTop =
+      document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (bodyScrollTop > headerOffset.top) {
+      stickyHeader.classList.add("active");
+    } else {
+      stickyHeader.classList.remove("active");
+    }
+  };
+};
+//ISOTOPE
+let btns = $(".servicos .button-group button");
+
+btns.click(function (e) {
+  $(".servicos .button-group button").removeClass("active");
+  e.target.classList.add("active");
+
+  let selector = $(e.target).attr("data-filter");
+  $(".servicos .grid").isotope({
+    filter: selector,
+  });
+});
+
+$(window).on("load", function () {
+  $(".servicos .grid").isotope({
+    filter: "*",
+  });
+});
+
+//MAGNIFY
+$(".grid .popup-link").magnificPopup({
+  type: "image",
+  gallery: {
+    enabled: true,
+    tPrev: "Anterior",
+    tNext: "Próxima",
+    tCounter: "%curr% de %total%",
+  },
+});
+
+//OWL
+
+
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 30,
+  autoplay: true,
+  autoplayTimeout: 6000,
+  dots: true,
+  lazyLoad: true,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
